c0b5e2a3158739499873f76e12363cc130e7f289
Avishka Shyaman <avishkashyaman@gmail.com>
2022-10-16 15:51:38 +0530

    added signup controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,43 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user.model');
+
 const login = async (req, res) => {
 };
 
 const signUp = async (req, res) => {
+  try {
+    const {
+      body: { email, password, name, role },
+    } = req;
+
+    const isUserExist = await User.findOne({ email });
+
+    if (isUserExist) {
+      throw 'Email "' + email + '" is already registered';
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashPassword,
+      role,
+    });
+
+    const user = await newUser.save();
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      data: error,
+    });
+  }
 };
 
 module.exports = {
