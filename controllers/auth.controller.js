9103bb29a133c290053802eed62fb96571036747
Avishka Shyaman <avishkashyaman@gmail.com>
2022-10-16 15:52:16 +0530

    Added login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,42 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
+const { getToken } = require('../services/auth');
 
 const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ status: false, message: 'Invalid Email' });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res
+        .status(401)
+        .json({ status: false, message: 'Invalid Password' });
+    }
+
+    const token = await getToken(user._id);
+
+    res.status(200).json({
+      success: true,
+      data: {
+        id: user.id,
+        name: user.name,
+        email,
+        role: user.role,
+        token,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 const signUp = async (req, res) => {
